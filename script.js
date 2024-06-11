95322471518a5d6ea89a5d4444790f40d20bfb9b
gcottar <guy.cottar@icloud.com>
2024-06-11 15:26:01 +0200

    update

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const text = "Guy COTTAR";
 let index = 0;
 
 function typeWriter() {
-    if (index < text.length) {
-        document.getElementById("typed-text").textContent += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, 120); // Ajustez la vitesse ici (en millisecondes)
-    }
+  if (index < text.length) {
+    document.getElementById("typed-text").textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 120); // Ajustez la vitesse ici (en millisecondes)
+  }
 }
 
 window.onload = typeWriter;
